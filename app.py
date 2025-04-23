55e18cc06f7272c0e73954797515fa0ea3fffc68
Екатерина <Chetverikova.k2003@mail.ru>
2025-04-23 14:35:56 +0500

    Добавлены тесты

--- app.py
+++ app.py
@@ -11,10 +11,20 @@ class Order(BaseModel):
     client: str
     status: str
 
-repo = []
+repo = [
+    Order(
+        number=1,
+        startDate=datetime.date (2000, 12, 1),
+        device="123",
+        problemType="123",
+        description="123",
+        client="123",
+        status="в ожидании"
+    )
+]
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/orders/")
 def get_order():
-    return "SHello"
+    return repo
