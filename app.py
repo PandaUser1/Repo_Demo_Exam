92022bc8b9adf5b4b9d266bfef19343e036cae94
Екатерина <Chetverikova.k2003@mail.ru>
2025-04-23 14:29:10 +0500

    Создание сущности заявки со всеми параметрами

---
+++ app.py
@@ -0,0 +1,18 @@
+import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Order(BaseModel):
+    number: int
+    startDate: datetime.date
+    device: str
+    problemType: str
+    description: str
+    client: str
+    status: str
+
+app = FastAPI()
+
+@app.get("/")
+def get_order():
+    return "SHello"
