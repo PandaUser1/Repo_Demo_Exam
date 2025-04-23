9378cd53ffa179c6cfd53b9b84d879fdf576a88b
Екатерина <Chetverikova.k2003@mail.ru>
2025-04-23 15:34:30 +0500

    Добавлена сущность DTO обновления заявки

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Annotated
+from typing import Annotated, Optional
 from fastapi import FastAPI, Form
 from pydantic import BaseModel
 
@@ -11,6 +11,13 @@ class Order(BaseModel):
     description: str
     client: str
     status: str
+    master: Optional[str] = "Не назначен"
+
+class UpdateOrderDTO(BaseModel):
+    number: int
+    status: Optional[str] = ""
+    description: Optional[str] = ""
+    master: Optional[str] = ""
 
 repo = [
     Order(
