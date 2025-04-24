03545cd73f443f01642a9d91087ee9d2c43a89b0
Екатерина <148535020+Nonameus8@users.noreply.github.com>
2025-04-24 20:17:29 +0500

    Добавлен фильтр по номеру и система уведомлений о смене статуса заявки

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 from typing import Annotated, Optional
 from fastapi import FastAPI, Form
+from fastapi import Query
 from pydantic import BaseModel
 
 class Order(BaseModel):
@@ -22,7 +23,25 @@ class UpdateOrderDTO(BaseModel):
 repo = [
     Order(
         number=1,
-        startDate=datetime.date (2000, 12, 1),
+        startDate=datetime.date(2000, 12, 1),
+        device="123",
+        problemType="123",
+        description="123",
+        client="123",
+        status="в ожидании"
+    ),
+    Order(
+        number=2,
+        startDate=datetime.date(2000, 12, 1),
+        device="123",
+        problemType="123",
+        description="123",
+        client="123",
+        status="в ожидании"
+    ),
+      Order(
+        number=3,
+        startDate=datetime.date(2000, 12, 1),
         device="123",
         problemType="123",
         description="123",
@@ -33,23 +52,29 @@ repo = [
 
 app = FastAPI()
 
+message = ""
+
 @app.get("/orders/")
-def get_order():
-    return repo
+def get_order(param: Optional[int] = Query(None)):
+    if param is not None:
+        filtered_orders = [order for order in repo if order.number == param]
+        return filtered_orders if filtered_orders else {"detail": "Order not found"}
+    return {"repo" :repo, "message": message}
 
 @app.post("/orders")
 def create_order(dto: Annotated[Order, Form()]):
     repo.append(dto)
 
 @app.post("/update")
-def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
+def update_order(dto: Annotated[UpdateOrderDTO, Form()]):
     for o in repo:
         if o.number == dto.number:
-            if dto.status != o.status and dto.status !="":
+            if dto.status != o.status and dto.status != "":
                 o.status = dto.status
+                message += "Статус заявки №{o.namber} изменен"
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
                 o.master = dto.master
             return o
-        return "Не найдено"
+    return "Не найдено"
